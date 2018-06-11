d42392ade291180e5f6684bc54d3fc61f566dc71
Tejas Shroff <tejasshroff24@gmail.com>
2018-06-11 18:46:17 -0400

    Major improvements to sound quality of footsteps, and adding of volumetric aound plugin

---
+++ Assets/Game/Scripts/FootstepSounds.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepSounds : MonoBehaviour {
+
+    public Transform _startPos;
+    public Transform _currentPos;
+    public Transform _player;
+    public Transform _playerArea;
+
+    public AudioSource _audioSource;
+    public AudioSource _audioSourceBackup;
+
+    public AudioClip[] _stepsWood1;
+    public AudioClip[] _stepsWood2;
+    public AudioClip[] _stepsWood3;
+    public AudioClip[] _stepsConcrete;
+    public AudioClip[] _stepsStairs;
+
+    private float _distanceFromStartPos;
+
+    public float _distanceToStepNormal;
+    public float _distanceToStepStairs;
+    private float _distanceToStep;
+
+    public bool _alterPitch;
+
+
+    public float heightOffGround;
+
+    private int layerMask = 1 << 18;
+
+    //public bool ifStep;
+
+    // Use this for initialization
+    void Start()
+    {
+        //_audioSource = GetComponent<AudioSource>();
+        _startPos.transform.position = _currentPos.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentPos.transform.position = new Vector3(_player.transform.position.x, heightOffGround + _playerArea.transform.position.y, _player.transform.position.z);
+        _distanceFromStartPos = Vector3.Distance(_currentPos.transform.position, _startPos.transform.position);
+
+        if (_distanceFromStartPos > _distanceToStep)
+        {
+            TakeStep();
+            //print("Taking step");
+        }
+    }
+
+    private void TakeStep()
+    {
+        _startPos.transform.position = new Vector3(_currentPos.transform.position.x, heightOffGround + _playerArea.transform.position.y, _currentPos.transform.position.z);
+
+        Ray ray = new Ray(_startPos.transform.position, Vector3.down);
+        RaycastHit hit;
+        Debug.DrawRay(_startPos.transform.position, Vector3.down, Color.green, 10f);
+
+        if (Physics.Raycast(ray, out hit, 5f, layerMask))
+        {
+            //print("hi there");
+            print(hit.transform.gameObject.tag);
+            switch (hit.transform.gameObject.tag)
+            {
+                case "Wood1":
+                    RandomSound(_stepsWood1);
+                    break;
+                case "Wood2":
+                    RandomSound(_stepsWood2);
+                    break;
+                case "Wood3":
+                    RandomSound(_stepsWood3);
+                    break;
+                case "Concrete":
+                    RandomSound(_stepsConcrete);
+                    break;
+                case "Stairs":
+                    RandomSound(_stepsStairs);
+                    break;
+            }
+
+            if (hit.transform.gameObject.tag == "Stairs")
+                _distanceToStep = _distanceToStepStairs;
+            else
+                _distanceToStep = _distanceToStepNormal;
+        }
+    }
+
+    private void RandomSound(AudioClip[] clips)
+    {
+        var randomInt = Random.Range(0, clips.Length - 1);
+
+        _audioSource.clip = clips[randomInt];
+
+        if (_alterPitch)
+            _audioSource.pitch = 1 + Random.Range(-.15f, .15f);
+
+        _audioSource.Play();
+    }
+}
---
+++ Assets/Game/Scripts/RandomSoundWaypoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundWaypoint : MonoBehaviour {
+
+    public AudioClip[] _clips;
+    private AudioSource _audioSource;
+    public Transform[] _waypoints;
+
+    private int _randomIntClip;
+    private int _randomIntPos;
+
+    private float _clipLength;
+
+	// Use this for initialization
+	void Start () {
+        _audioSource = GetComponent<AudioSource>();
+        Randomize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_clipLength <= 0)
+            Randomize();
+        else
+            _clipLength -= Time.deltaTime;
+    }
+
+    private void Randomize()
+    {
+        _randomIntClip = Random.Range(0, _clips.Length - 1);
+        _randomIntPos = Random.Range(0, _waypoints.Length - 1);
+
+        var randomPos = _waypoints[_randomIntPos].transform.position;
+        var randomClip = _clips[_randomIntClip];
+        var randomDelay = Random.Range(0.5f, 1.5f);
+
+        transform.position = randomPos;
+        _clipLength = randomClip.length + randomDelay;
+        _audioSource.clip = randomClip;
+        _audioSource.PlayDelayed(randomDelay);
+    }
+}
