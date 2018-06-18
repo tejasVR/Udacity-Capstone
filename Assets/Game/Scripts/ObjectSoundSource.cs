9f4d29ff955d9a64b70008239a7d4bfdc023a24f
Tejas Shroff <tejasshroff24@gmail.com>
2018-06-18 17:43:15 -0400

    Assiging rigidbodies to movable objects, with object sound source scripts

---
+++ Assets/Game/Scripts/ObjectSoundSource.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSoundSource : MonoBehaviour {
+
+
+
+    private AudioSource _audioSource;
+
+    public enum ObjectType
+    {
+        cup,
+        glass,
+        chair,
+        wood,
+        metal
+    }
+
+    public ObjectType _objectType;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (_objectType)
+        {
+            case ObjectType.cup:
+                PlaySound.PlayAudioFromSelection(_audioSource, ObjectSounds.instance._cupSounds, true, -.05f, .05f);
+                break;
+
+            case ObjectType.glass:
+                PlaySound.PlayAudioFromSelection(_audioSource, ObjectSounds.instance._glassSounds, true, -.05f, .05f);
+                break;
+
+            case ObjectType.chair:
+                PlaySound.PlayAudioFromSelection(_audioSource, ObjectSounds.instance._chairSounds, true, -.05f, .05f);
+                break;
+
+            case ObjectType.wood:
+                PlaySound.PlayAudioFromSelection(_audioSource, ObjectSounds.instance._woodSounds, true, -.15f, .15f);
+                break;
+
+            case ObjectType.metal:
+                PlaySound.PlayAudioFromSelection(_audioSource, ObjectSounds.instance._metalSounds, true, -.05f, .05f);
+                break;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/ObjectSounds.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSounds : MonoBehaviour {
+
+    public static ObjectSounds instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //private AudioSource _audioSource;
+
+    //public enum ObjectType
+    //{
+    //    cup,
+    //    glass,
+    //    chair,
+    //    wood,
+    //    metal
+    //}
+
+    //public ObjectType _objectType;
+
+    public AudioClip[] _cupSounds;
+    public AudioClip[] _glassSounds;
+    public AudioClip[] _chairSounds;
+    public AudioClip[] _woodSounds;
+    public AudioClip[] _metalSounds;
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    switch (_objectType)
+    //    {
+    //        case ObjectType.cup:
+    //            PlaySound.PlayAudioFromSelection(_audioSource, _cupSounds, true, -.05f, .05f);
+    //            break;
+
+    //        case ObjectType.glass:
+    //            PlaySound.PlayAudioFromSelection(_audioSource, _glassSounds, true, -.05f, .05f);
+    //            break;
+
+    //        case ObjectType.chair:
+    //            PlaySound.PlayAudioFromSelection(_audioSource, _chairSounds, true, -.05f, .05f);
+    //            break;
+
+    //        case ObjectType.wood:
+    //            PlaySound.PlayAudioFromSelection(_audioSource, _woodSounds, true, -.05f, .05f);
+    //            break;
+
+    //        case ObjectType.metal:
+    //            PlaySound.PlayAudioFromSelection(_audioSource, _metalSounds, true, -.05f, .05f);
+    //            break;
+    //    }
+    //}
+
+}
