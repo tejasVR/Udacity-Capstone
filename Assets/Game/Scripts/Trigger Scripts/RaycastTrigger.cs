004c06cb413d13a0168e53d8c44a5735b4ba0869
Tejas Shroff <tejasshroff24@gmail.com>
2018-06-23 12:48:46 -0400

    Better zombie placement, and new shadow zombie trigger in hallway #1

---
+++ Assets/Game/Scripts/Trigger Scripts/RaycastTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastTrigger : MonoBehaviour {
+
+    public GameObject[] _objsToDisable;
+
+    public Transform _raycastToObj;
+
+    Vector3 _direction;
+
+    public float _angleToDisable;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        _direction = _raycastToObj.transform.position - PlayerScript._playerEye.transform.position;
+
+        if(!Physics.Raycast(PlayerScript._playerEye.transform.position, _direction))
+        {
+            if (Vector3.Angle(PlayerScript._playerEye.transform.forward, _direction) < _angleToDisable)
+            {
+                DisableObj();
+            }
+        }
+		
+	}
+
+
+    private void DisableObj()
+    {
+        foreach (var obj in _objsToDisable)
+        {
+            obj.SetActive(false);
+        }
+
+        this.gameObject.SetActive(false);
+    }
+
+}
